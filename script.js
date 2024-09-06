79d83bb3f1d046b0f3f9f6ce6f9367612849cea4
macn1 <athulraihan27@gmail.com>
2024-09-06 19:52:15 +0530

    rotation

--- script.js
+++ script.js
@@ -13,9 +13,24 @@ $(window).scroll(function() {
     let bgPos = ((scrollPos / bgwidth) * 100)/($(".bg-1").attr("speed"));
     $(".bg-1").css("background-position", bgPos + "% 50%");
 });
+
 $(window).scroll(function() {
     let bgwidth = window.screen.width;
     let scrollPos =  $(window).scrollTop() - $(".bg-2").offset().top*2;
     let bgPos = 100 + ((scrollPos / bgwidth) * 100)/($(".bg-2").attr("speed"));
     $(".bg-2").css("background-position", bgPos + "% 50%");
+});
+
+$(window).scroll(function() {
+    let bgwidth = window.screen.width;
+    let scrollPos =  $(window).scrollTop() - $(".bg-4").offset().top*2;
+    let bgPos = 100 + ((scrollPos / bgwidth) * 100)/($(".bg-4").attr("speed"));
+    $(".bg-4").css("background-position", bgPos + "% 50%");
+});
+
+$(window).scroll(function() {
+    let bgwidth = window.screen.width*1.2;
+    let scrollPos = $(this).scrollTop();
+    let bgPos = ((scrollPos / bgwidth) * 100)/($(".bg-3").attr("speed"));
+    $(".bg-3").css("background-position", bgPos + "% 50%");
 });
